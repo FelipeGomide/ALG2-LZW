a611bb2dbd1a968373adbc63932eaecee3e80f53
Felipe Gomide <flgomide@gmail.com>
2024-11-18 16:19:56 -0300

    Adicionada interface em main, estático parametrizado

--- lzw/static.py
+++ lzw/static.py
@@ -1,35 +1,37 @@
 from bitstring import *
-from trie.binary_compact_trie import Trie
+from .trie.binary_compact_trie import Trie
 
 import sys
+import os
 
 def countTotalBits(num):
      return len(bin(num)[2:])
 
-def lzw_compress():
+def lzw_static_compress(input_file, max_dict_size):
     print(sys.argv, len(sys.argv))
 
     dictionary = Trie()
 
+    compressed_file = os.path.splitext(input_file)[0] + ".ceflzw"
+
     # Read binary content of the file
-    c = BitStream(filename='input/casmurro.txt')
-    file = open("compressed.txt", mode="wb")
+    c = BitStream(filename=f'input/{input_file}')
+    file = open(f"compressed/{compressed_file}", mode="wb")
 
-    #print(len(c), len(c.b))
-
-    """print("Arquivo codificado em string binária: ", c.bin)
-    print("Tamanho: ", len(c))
-    print("Bytes: ", len(c) // 8)
-    print("BitStream size: ", sys.getsizeof(c))
-    print("Binary String size: ", sys.getsizeof(c.bin))"""
+    # """print("Arquivo codificado em string binária: ", c.bin)
+    # print("Tamanho: ", len(c))
+    # print("Bytes: ", len(c) // 8)
+    # print("BitStream size: ", sys.getsizeof(c))
+    # print("Binary String size: ", sys.getsizeof(c.bin))"""
 
     i = 0
     idx = 0
-    n_bits = 9
-    result = []  # Stores the resulting encoded string
+    #n_bits = 9
+
+    #result = []  # Stores the resulting encoded string
     to_file = BitStream()
     
-    max_bits_code = 9
+    #max_bits_code = 9
     
 
     # Preenche tabela unicode
@@ -41,7 +43,6 @@ def lzw_compress():
     w = c.read(8)
     i += 8
 
-
     while i < len(c):
         # Read the next byte (8 bits) as a binary string
         symbol = c.read(8)
@@ -50,23 +51,16 @@ def lzw_compress():
         #print("Decode_byte", decode_byte, chr(decode_byte.uint))
         #print("--> " + decode_byte)
 
-        if countTotalBits(idx) > max_bits_code:
+        if countTotalBits(idx) > max_dict_size:
             # Limit exceeded: Only add the current code to the result
             print("Limite de bits excedido!")
             found, code = dictionary.find(w.b)
             if found:
-                to_append = Bits(f'uint{n_bits}={code}')
-                result.append(to_append.b)
+                to_append = Bits(f'uint{max_dict_size}={code}')
+                #result.append(to_append.b)
                 to_file.append(to_append)
             
             w = symbol
-            
-            # found, code_bits = dictionary.find(symbol.b)
-            # if found:
-            #     #result.append(code_bits)  # Append the code to the result
-            #     to_append = Bits(f'uint{n_bits}={code}')
-            #     to_file.append(to_append)
-            #     result.append(to_append.b)
 
         else:
             wc = Bits(bin = w.b + symbol.b)
@@ -75,7 +69,7 @@ def lzw_compress():
             #print("Buscado:", wc.b, found , code_bits)
 
             if found:
-                w = Bits(bin = wc.b)
+                w = wc
                 #print("match!")
                 #result.append(code_bits)
 
@@ -88,62 +82,58 @@ def lzw_compress():
                 # Insert the new sequence in the Trie
                 dictionary.insert(wc.b, idx)
                 idx += 1
-
                 # to_append = Bits(f'uint{n_bits}={w.uint}')
-                boolean, code = dictionary.find(w.b)
-                #print(w.b, w.uint, boolean, code)
-                to_append = Bits(f'uint{n_bits}={code}')
 
-                #print("To_append", boolean, to_append)
+                _, code = dictionary.find(w.b)
+                
+                to_append = Bits(f'uint{max_dict_size}={code}')
                 to_file.append(to_append)
-                result.append(to_append.b)  # Append the previous match
+                #result.append(to_append.b)  # Append the previous match
 
-                w = Bits(bin = symbol.b)  # Start a new sequence
+                w = symbol  # Start a new sequence
 
         # Increment the position by step size
         i += 8
-        #print(i)
-        #dictionary.print()
 
     # Add any remaining sequence to the result
     if w:
         found, code = dictionary.find(w.b)
         if found:
-            to_append = Bits(f'uint{n_bits}={code}')
-            result.append(to_append.b)
+            to_append = Bits(f'uint{max_dict_size}={code}')
+            #result.append(to_append.b)
             to_file.append(to_append)
 
-    #print("Encoded Result:", result)
-    #dictionary.print()
-    #print(to_file.bin)
     to_file.tofile(file)
     file.close()
 
-    return result
+    return compressed_file
 
 
-def lzw_decompress(compressed):
+def lzw_static_decompress(compressed_file, max_dict_size):
     dictionary = Trie()
     idx = 0
 
-    c = BitStream(filename='compressed.txt')
-    file = open("decompressed.txt", mode="wb")
+    out_file = os.path.splitext(compressed_file)[0] + ".txt"
+
+
+    c = BitStream(filename=f'compressed/{compressed_file}')
+    file = open(f"decompressed/{out_file}", mode="wb")
     #print(c.bin)
 
     # Preenche tabela unicode
     for i in range(256):
-        dictionary.insert(Bits(uint9=i).b, Bits(uint8=i).b)
+        dictionary.insert(Bits(f'uint{max_dict_size}={i}').b, Bits(uint8=i).b)
         idx += 1
 
     #dictionary.print()
     
     i = 0
-    passo = 9
-    max_bits_code = 9
-    n_bits = 9
+    #passo = 9
+    #max_bits_code = 9
+    #n_bits = 9
 
     result = ""
-    code = c.read(9).b 
+    code = c.read(max_dict_size).b 
     boolean, string = dictionary.find(code)
     
     to_append = Bits(b = string)
@@ -153,7 +143,7 @@ def lzw_decompress(compressed):
 
     while True:
         try:
-            code = c.read(9).b
+            code = c.read(max_dict_size).b
         except:
             break
         
@@ -166,7 +156,7 @@ def lzw_decompress(compressed):
         if found:
             entry = search
             
-        elif code == Bits(uint9=idx).b:
+        elif code == Bits(f"uint{max_dict_size}={idx}").b:
             entry = string + string[:8]
             
         else:
@@ -179,8 +169,8 @@ def lzw_decompress(compressed):
         to_append.tofile(file)
         result += str(entry)
 
-        if idx <= 2**(max_bits_code)-1:
-            dictionary.insert(Bits(uint9=idx).b, string+entry[:8])
+        if idx <= 2**(max_dict_size)-1:
+            dictionary.insert(Bits(f"uint{max_dict_size}={idx}").b, string+entry[:8])
         
         string = entry
 
@@ -190,14 +180,4 @@ def lzw_decompress(compressed):
     return result
 
 if __name__ == '__main__':
-    compressed = lzw_compress()
-    #print(compressed)
-
-    # for c in compressed:
-    #     number = Bits(bin = c).uint
-    #     if number < 255:
-    #         print(chr(number), )
-    #     else: print(number)
-
-    decompressed = lzw_decompress(compressed)
-    #print(decompressed)
+    print("Módulo apenas de import executado como código")
--- main.py
+++ main.py
@@ -1,61 +1,76 @@
-from lzw.trie.binary_compact_trie import Trie
-from lzw.static import lzw_compress
+#from lzw.trie.binary_compact_trie import Trie
+from lzw.static import lzw_static_compress, lzw_static_decompress
 
-from bitstring import *
+#from bitstring import *
 
 import sys
 
+compress = 0
+decompress = 0
+dynamic_mode = False
+min_dict_size = 8
+max_dict_size = 12
+file = "input/input.txt"
+
 def main():
-    print(sys.argv, len(sys.argv))
+    parse_args()
 
-    mydict = Trie()
-    mydict.insert('010', 3)
-    mydict.insert('0101', 1)
-    mydict.insert('0111', 2)
-    mydict.insert('0', 4)
-    mydict.print()
+    print("Configurações", "\nDynamic", dynamic_mode, "\nMin", min_dict_size)
+    print("Max", max_dict_size, "\nFilename", file)
+    print('Compress', compress, '\nDecompress', decompress)
+
+    if compress and decompress:
+        out = lzw_static_compress(file, max_dict_size)
+        lzw_static_decompress(out, max_dict_size)
+    else:
+        if compress:
+            lzw_static_compress(file, max_dict_size)
+
+        if decompress:
+            lzw_static_decompress(file, max_dict_size)
+        
 
 
-    teste = Trie()
+def parse_args():
+    args = sys.argv
+    print(args, len(args))
 
-    c = BitStream(filename='test.txt')
+    if (len(args) < 2):
+        raise "Not enough arguments!"
 
-    new = open('reescrita.txt', mode='wb')
+    global file
+    global min_dict_size
+    global max_dict_size
+    global dynamic_mode
+    global decompress, compress
 
-    print("Arquivo codificado em string binária: ", c.bin)
-    print("Tamanho: ", len(c))
-    print("Bytes: ", len(c)//8)
-    print("BitStream size: ", sys.getsizeof(c))
-    print("Binary String size: ", sys.getsizeof(c.bin))
+    file = args[1]
 
-    i = 0
-    idx = 0
-    passo = 8
-    result = ''
-    while i < len(c):
-        decode_byte = c.read(8)
-        teste.insert(decode_byte.b, idx)
-        i += passo
-        idx += 1
+    for i in range(len(args)):
+        match args[i]:
+            case '--static':
+                dynamic_mode = False
+            case '--dynamic':
+                dynamic_mode = True
+            case '--max_size':
+                max_dict_size = int(args[i+1])
+            case '--min_size':
+                min_dict_size = int(args[i+1])
+            case '--compress' | '-c':
+                compress = 1
+            case '--decompress' | '-d':
+                decompress = 1
+            case '--both' | '-b':
+                decompress = 1
+                compress = 1
+            case _:
+                pass
+    
+    return
 
-        decode_byte.tofile(new)
 
-        result += chr(decode_byte.uint)
 
-    new.close()
-
-    print(result)
-    teste.print()
-
-    # to_search = Bits(uint=ord('f'), length=8)
-    # a = teste.find(to_search.b)
-    # print(a, to_search.b)
-
-    compressed = lzw_compress()
-    print(compressed)
 
 
 if __name__ == '__main__':
     main()
-
-#decompressed = lzw_decompress(compressed)
